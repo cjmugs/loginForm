1f5a3cbfb520872efe6f7b576903d621e558bae5
cjmugs <cjmugs@gmail.com>
2020-10-27 10:43:54 -0500

    updates

--- loginForm/loginForm/urls.py
+++ loginForm/loginForm/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Form/', include('Form.urls')),
+    path('form/', include('Form.urls')),
 ]
