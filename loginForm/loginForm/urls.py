e708eac77baf9d9262797f80a509fce35f60d375
cjmugs <cjmugs@gmail.com>
2020-10-29 14:37:24 -0500

    updates

--- loginForm/loginForm/urls.py
+++ loginForm/loginForm/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('form/', include("Form.urls")),
     path('main/', include("main.urls")),
-    path('users/', include("users.urls")),
+    path('', include("users.urls")),
 ]
